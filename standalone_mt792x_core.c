ffe5309cc7c52ad668b519e4ed8e6cbe397e250b
Sam Bélanger <astromangamingofficial@gmail.com>
2026-06-21 10:32:47 -0400

    Fix

--- standalone_mt792x_core.c
+++ standalone_mt792x_core.c
@@ -800,6 +800,11 @@ out:
 	return offload_caps;
 }
 
+static bool standalone_mt792x_needs_cnm_runtime(const void *drv_data)
+{
+	const char *fw_wm = drv_data;
+}
+
 struct ieee80211_ops *
 standalone_mt792x_get_mac80211_ops(struct device *dev,
 			const struct ieee80211_ops *mac80211_ops,
